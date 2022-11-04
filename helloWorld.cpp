0fb499029e7cd44df8f554890de73649180dcb3f
rikardgn <106836233+rikardgn@users.noreply.github.com>
2022-11-04 16:11:38 +0100

    Add files via upload
    A basic program that teaches ytou how to write hello world in the screen :)

---
+++ helloWorld.cpp
@@ -0,0 +1,7 @@
+//This program shows basic i/o and how to write an hello world program in C++
+using namespace std;
+#include <iostream>
+int main(void){
+    cout << "Hello world! I'm learning C++!" << "\n";
+    return 0;
+}
