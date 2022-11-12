302cfc64d0bae4e3c5f20c1a548922d4de91ad60
rikardgn <106836233+rikardgn@users.noreply.github.com>
2022-11-12 12:04:47 +0100

    Add files via upload
    Shows how to use floating point numbers in C++.

---
+++ floatEx.cpp
@@ -0,0 +1,11 @@
+//This examples demonstrates how to use floatpingpoint numbers in C++
+#include <iostream>
+using namespace std;
+int main(void){
+    float floatA=5.5, floatB=10.8, floatC;
+    floatC=floatA+floatB;
+    cout << floatA << "\n";
+    cout << floatB << "\n";
+    cout << "Sum of A and B:" << "\n";
+    cout << "Float C =" << floatC << "\n";
+}
