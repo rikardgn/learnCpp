08e6ad176cd65318c4c2e8e23245265ad3bb6297
Rikard Grossman-Nielsen <106836233+rikardgn@users.noreply.github.com>
2023-03-11 15:04:19 +0100

    Add files via upload
    A program that show to use int vars to make simple calculations.

---
+++ intEx.cpp
@@ -0,0 +1,15 @@
+//A simple program that shows how to use integer variables to make simple calculations
+#include <iostream>
+using namespace std;
+int main(void){
+    int numA, numB, sum;
+    numA=10;
+    numB=15;
+    cout << "Value of numA:";
+    cout << numA << "\n";
+    cout << "Value of numB:";
+    cout << numB << "\n";
+    sum=numA+numB;
+    cout << "Value of sum:";
+    cout << sum << "\n";
+}
