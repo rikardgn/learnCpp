47d2e79aee1b0aaef83faa85af2e783f06b3062d
Rikard Grossman-Nielsen <106836233+rikardgn@users.noreply.github.com>
2025-06-07 17:52:57 +0200

    Add files via upload
    Example of how to use concepts in c++ 20. This example uses simple numerical computations to illustrate this concept.

---
+++ conceptEx1.cpp
@@ -0,0 +1,26 @@
+//Concepts are used to give templates constraints. Concepts amongst other things makes template code more readable.
+//It also produces compiler errors that are more "human friendly".
+//Rikard Grossman-Nielsen
+//4th June 2025
+#include <numeric>
+#include <vector>
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::signed_integral<T>||std::floating_point<T>
+//function that returns the sum of a vector divided by 2
+constexpr double sum_div_2(std::vector<T> const &numberField){
+    const double sumField = std::accumulate(numberField.begin(), numberField.end(), 0.0);
+    return sumField/2;
+}
+
+int main(){
+    std::vector numSignedInt {1, -2, 8, 4, -6};
+    std::vector numFloat {1.2, 4.5, -3.6, 4.5, 4.123};
+    std::cout << "Sum of vector with ints divided by 2: " << "\n" << sum_div_2(numSignedInt) << "\n";
+    std::cout << "Sum of vector with ints divided by 2: " << "\n" << sum_div_2(numFloat) << "\n";
+
+    return 0;
+}
+
