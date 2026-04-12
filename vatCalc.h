b7e20db449babfdbb7fe1cae2a998c2fb0705f79
Rikard Grossman-Nielsen <106836233+rikardgn@users.noreply.github.com>
2026-04-12 22:28:31 +0200

    Add files via upload
    Example of header file that contains function definitions for functions that calculate different percentages of VAT dependent upon type of item

---
+++ vatCalc.h
@@ -0,0 +1,8 @@
+//header contaning function definitions for making VAT calcualtions
+double vatBook(double price);
+
+double vatMovie(double price);
+
+double vatElectronics(double price);
+
+double vatCars(double price);
